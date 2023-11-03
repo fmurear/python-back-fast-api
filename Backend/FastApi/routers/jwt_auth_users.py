e91b73ddd37c40858f03ab072efbe752ff5d752a
fmurear <pakito_699@hotmail.com>
2023-11-03 14:18:13 +0000

    Inicio autenticación jwt

---
+++ Backend/FastApi/routers/jwt_auth_users.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
+
+ALGORITHM = "HS256"
+
+app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes=["bcrypt"])
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+class UserDB(User):
+    password: str
+
+users_db = {
+    "mouredev": {
+        "username": "mouredev",
+        "full_name": "Brais Moure",
+        "email": "braismoure@mouredev.com",
+        "disabled": False,
+        "password": "$2a$12$5BQbrVdHuf3f/TMr3P/8Z.Zih9lZW2A8d8a/t6ckBy.pg4E.euuY6"
+    },
+    "mouredev2": {
+        "username": "mouredev2",
+        "full_name": "Brais Moure 2",
+        "email": "braismoure2@mouredev.com",
+        "disabled": True,
+        "password": "$2a$12$Ucx1U3I0/k7q9uLSG9feDuzg0WE1kOyw4TUEHGF..7kvemVvi6XrC"
+    }
+}
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto"
+        )
+    
+    user = search_user_db(form.username)
+
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta"
+        )
+    
+    return {"access_token": user.username, "token_type": "bearer"}
