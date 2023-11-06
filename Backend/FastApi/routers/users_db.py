c5ad59f8753330078b11831630871fdc4af9bb51
fmurear <pakito_699@hotmail.com>
2023-11-06 18:28:11 +0000

    Creación CRUD con MongoDB.

---
+++ Backend/FastApi/routers/users_db.py
@@ -0,0 +1,72 @@
+"""Módulos a importar"""
+from fastapi import APIRouter, HTTPException, status
+from db.models.user import User
+from db.client import db_client
+from db.schemas.user import user_schema, users_schema 
+from bson import ObjectId
+
+router = APIRouter(prefix="/userdb",
+                   tags=["userdb"],
+                   responses={status.HTTP_404_NOT_FOUND : {"message": "No encontrado"}})
+
+users_list = []
+
+@router.get("/", response_model= list[User])
+async def users():
+    """Función que devuelve todos los usuarios"""
+    return users_schema(db_client.local.users.find())
+
+# Path y Query param
+@router.get("/{id}")
+async def user_path(id: str):
+    """Función que devuelve todos los usuarios por id"""
+    return search_user('_id', ObjectId(id))
+
+#Query param    
+@router.get("/query/")
+async def user_query(id: str):
+    """Función que devuelve todos los usuarios por id"""
+    return search_user('_id', ObjectId(id))
+
+@router.post("/", response_model= User, status_code=status.HTTP_201_CREATED )
+async def user(user: User):
+    """Función para añadir usuario"""
+    if type(search_user('email',user.email)) == User:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    
+    user_dict = dict(user)
+    del user_dict["id"]
+
+    id = db_client.local.users.insert_one(user_dict).inserted_id
+
+    new_user = user_schema(db_client.local.users.find_one({"_id": ObjectId(id)}))
+
+    return User(**new_user)
+
+@router.put("/")
+async def user(user: User):
+    """Función para actualizar el usuario"""
+    user_dict = dict(user)
+    del user_dict["id"]
+    try:
+        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    print(user.id)
+    return search_user('_id',ObjectId(user.id))
+
+@router.delete("/{id}")
+async def user(id: str, status_code=status.HTTP_204_NO_CONTENT):
+    """Función para eliminar un usuario"""
+    found = db_client.local.users.find_one_and_delete({"_id":ObjectId(id)})
+    
+    if not found:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario no existe")
+
+def search_user(field: str, key):
+    """Función que devuelve todos los usuarios por id y si no existe retorna un error"""
+    try:
+        return User(**user_schema(db_client.local.users.find_one({field: key})))
+    except:
+        return {"error": "El usuario ya existe"}
+    
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # python-back-fast-api
 Backend de python con fast api y uvicorn.
 
+## Server
 Utilizar para lanzar uvicorn main:app --reload --host 127.0.0.1 --port 33336 dentro de la carpeta Backend/FastApi/
 donde main es el módulo y app es el nombre de la variable en la que iniciamos el FastAPI.
+
+## BD
+Utilizamos mongodb. Para arrancar el servicio escribir el comando mongod.
